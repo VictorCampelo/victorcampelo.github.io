dc1353f9b3942cfb06efb5f2232da89294360d95
Victor Campelo <victor_campelo@outlook.com>
2020-10-19 18:20:57 -0300

    Updates

---
+++ precache-manifest.5c546bc3735056b48760bcce6bd56ed9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a32841f88ea32d84d4325a7e218598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ad111c713ad98ecefa",
+    "url": "/static/css/main.581e8272.chunk.css"
+  },
+  {
+    "revision": "edb28349ff867ec6cea6",
+    "url": "/static/js/2.4007c75a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4007c75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ad111c713ad98ecefa",
+    "url": "/static/js/main.6c9f6e45.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8df826577a002ff7d71e4613fcfa4123",
+    "url": "/static/media/resume.8df82657.pdf"
+  },
+  {
+    "revision": "a76c5dfd4673122704e9aa206f9ee747",
+    "url": "/static/media/victor.a76c5dfd.jpg"
+  }
+]);
