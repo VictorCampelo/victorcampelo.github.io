01679c8f79eb8a9b5c1f72627c3aa15eafd2f7c1
Victor Campelo <victor_campelo@outlook.com>
2020-10-20 17:50:56 -0300

    change favicon and build project

---
+++ build/precache-manifest.1f4d31d7621ef04538e8e48ae4cf3b9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a337d86252f9624e9f0f056ff7456e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10a530343aae78686a85",
+    "url": "/static/css/main.581e8272.chunk.css"
+  },
+  {
+    "revision": "edb28349ff867ec6cea6",
+    "url": "/static/js/2.4007c75a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4007c75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a530343aae78686a85",
+    "url": "/static/js/main.9efdba76.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8df826577a002ff7d71e4613fcfa4123",
+    "url": "/static/media/resume.8df82657.pdf"
+  },
+  {
+    "revision": "a76c5dfd4673122704e9aa206f9ee747",
+    "url": "/static/media/victor.a76c5dfd.jpg"
+  }
+]);
